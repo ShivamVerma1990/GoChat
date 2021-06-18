0b82bfa627767156217513cb176f8e9c023f8db7
ShivamVerma1990 <71723717+ShivamVerma1990@users.noreply.github.com>
2021-06-18 18:18:08 +0530

    very initial commit

---
+++ app/src/main/java/com/candroid/gochat/SignUp.kt
@@ -0,0 +1,11 @@
+package com.candroid.gochat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
